2458ea672148e933d90a93b2a2e12ac0414f1e58
User <kim4501@gmail.com>
2023-02-05 16:31:12 +0800

    update README

--- tenthirty/student/tenthirty.v
+++ tenthirty/student/tenthirty.v
@@ -35,9 +35,8 @@ end
 //================================================================
 //   REG/WIRE
 //================================================================
-//seg7_temp
 reg [7:0] seg7_temp[0:7];
-
+reg [2:0] dis_cnt;
 
 //================================================================
 //   FSM
@@ -60,6 +59,58 @@ wire [3:0] number;
 //   SEGMENT
 //================================================================
 
+//seg7_temp
+
+//display counter 
+always@(posedge dis_clk or negedge rst_n) begin
+    if(!rst_n) begin
+        dis_cnt <= 0;
+    end
+    else begin
+        dis_cnt <= (dis_cnt >= 7) ? 0 : (dis_cnt + 1);
+    end
+end
+
+always @(posedge dis_clk or negedge rst_n) begin 
+    if(!rst_n) begin
+        seg7 <= 8'b0000_0001;
+    end 
+    else begin
+        if(!dis_cnt[2]) begin
+            seg7 <= seg7_temp[dis_cnt];
+        end
+    end
+end
+
+always @(posedge dis_clk or negedge rst_n) begin 
+    if(!rst_n) begin
+        seg7_l <= 8'b0000_0001;
+    end 
+    else begin
+        if(dis_cnt[2]) begin
+            seg7_l <= seg7_temp[dis_cnt];
+        end
+    end
+end
+
+always@(posedge dis_clk or negedge rst_n) begin
+    if(!rst_n) begin
+        seg7_sel <= 8'b11111111;
+    end
+    else begin
+        case(dis_cnt)
+            0 : seg7_sel <= 8'b00000001;
+            1 : seg7_sel <= 8'b00000010;
+            2 : seg7_sel <= 8'b00000100;
+            3 : seg7_sel <= 8'b00001000;
+            4 : seg7_sel <= 8'b00010000;
+            5 : seg7_sel <= 8'b00100000;
+            6 : seg7_sel <= 8'b01000000;
+            7 : seg7_sel <= 8'b10000000;
+            default : seg7_sel <= 8'b11111111;
+        endcase
+    end
+end
 
 //================================================================
 //   LED
